ff358b35a267b1552cbdff6867cec66be1d647f6
Shakthi Jayan J <shakthijayan19@gmail.com>
2026-09-13 13:34:28 +0530

    Added dto, mapper and service layer

--- patient-service/src/main/java/com/shakthi/patientservice/model/Patient.java
+++ patient-service/src/main/java/com/shakthi/patientservice/model/Patient.java
@@ -22,6 +22,9 @@ public class Patient {
     private String email;
 
     @NotNull
+    private String address;
+
+    @NotNull
     private LocalDate dateOfBirth;
 
     @NotNull
@@ -65,5 +68,13 @@ public class Patient {
 
     public void setRegisteredDate(LocalDate registeredDate) {
         this.registeredDate = registeredDate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
---
+++ patient-service/src/main/java/com/shakthi/patientservice/service/PatientService.java
@@ -0,0 +1,23 @@
+package com.shakthi.patientservice.service;
+
+import com.shakthi.patientservice.dto.PatientResponseDTO;
+import com.shakthi.patientservice.mapper.PatientMapper;
+import com.shakthi.patientservice.model.Patient;
+import com.shakthi.patientservice.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PatientService {
+    private final PatientRepository patientRepository;
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+    public List<PatientResponseDTO> patientResponseDTOS() {
+        List<Patient> patients = patientRepository.findAll();
+        return patients.stream().
+                map(PatientMapper::toDTO).toList();
+    } 
+}
